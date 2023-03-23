74e7f05e5faf89491bbd5139433606c304d1671d
Shashank Mishra <mishra.shashank35@gmail.com>
2023-03-23 19:41:36 +0530

    Add basic UI element test case

--- AstronomyNasaUITests/AstronomyNasaUITests.swift
+++ AstronomyNasaUITests/AstronomyNasaUITests.swift
@@ -8,34 +8,23 @@
 import XCTest
 
 final class AstronomyNasaUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func test_UIElements() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(iOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        
+        let textViewQueries = app.textViews
+        XCTAssertTrue(textViewQueries.element.exists, "TextView not found")
+        
+        let staticTextQueries = app.staticTexts
+        XCTAssertTrue(staticTextQueries.element.exists, "Title Label not found")
+        
+        let imageViewElement = app.windows.children(matching: .other).element
+        
+        XCTAssertTrue(imageViewElement.exists, "ImageView not found")
     }
 }
