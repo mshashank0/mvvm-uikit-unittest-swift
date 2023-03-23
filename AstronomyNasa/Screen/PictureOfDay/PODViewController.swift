c8bc9d5a796132255e05cdcd2af6e8365147481f
Shashank Mishra <mishra.shashank35@gmail.com>
2023-03-23 16:09:17 +0530

    Add view components and present it on view controller

--- AstronomyNasa/Screen/PictureOfDay/PODViewController.swift
+++ AstronomyNasa/Screen/PictureOfDay/PODViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class PODViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UITextView!
+    @IBOutlet var imageView: UIImageView!
 
     var viewModel: PODViewModel?
 
@@ -19,10 +23,18 @@ class PODViewController: UIViewController {
         super.viewDidLoad()
         
         // Observer for getting pod object update
-        viewModel?.pod.bind({ val in
-            debugPrint(val)
+        viewModel?.pod.bind({ [weak self] _ in
+            self?.presentDataOnView()
         })
         
         viewModel?.getPictureOfDay()
+    }
+    
+    func presentDataOnView() {
+        DispatchQueue.main.async { [unowned self] in
+            // TODO: - Get image and set it on imageView
+            titleLabel.text = viewModel?.pod.value?.title ?? ""
+            descriptionLabel.text = viewModel?.pod.value?.explanation ?? ""
+        }
     }
 }
